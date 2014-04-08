1f2539786945e804667d14792e12a91aad23ac92
Roman Golub <roman.golub@creuna.no>
2014-04-08 17:00:00 +0300

    Ethalon can be downloaded

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MockupDiffHelper.Downloader;
 
 namespace ConsoleApp
 {
     class Program
     {
+        public static App _app;
+
         static void Main(string[] args)
         {
+            _app = new App();
+
+            _app.TestDownloadMockup();
+        }
+    }
+
+    public class App
+    {
+        public void TestDownloadMockup()
+        {
+            var ethalonUrl = "file:///D:/PROJECTS/OSL-BW-Offshore/html-mockup/converted-html/index.html";
+            var applicantUrl = string.Empty;
+            var dataFolderPath = @"D:\PROJECTS\MockupDiffHelper\Data\";
+
+            var downloadedDocumentPath = @"D:\PROJECTS\MockupDiffHelper\Data\BW.Offshore\FrontPage\Ethalon\Original\index.html";
+
+            var helper = new Downloader(ethalonUrl, applicantUrl, dataFolderPath);
+
+            helper.Download(ethalonUrl, downloadedDocumentPath);
         }
     }
 }
